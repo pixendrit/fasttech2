82500c8488363a7203adfff24312bf96289f3bee
Endrit <pixendrit@outlook.com>
2022-01-10 03:29:36 +0100

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -17,35 +17,23 @@ export function Footer() {
         <div className={logos}>
           <Logo />
         </div>
-        <strong>gatsby-starter-shopify</strong> change this by editing{" "}
-        <code>src/components/footer.jsx</code>
       </div>
       <nav className={links} aria-label="footer">
         <ul className={footerNavList}>
           <li className={footerNavListItem}>
-            <a href="https://github.com/gatsbyjs/gatsby-starter-shopify">
-              Source Code and Docs
+            <a href="https://www.facebook.com/fasttechferizaj">
+              Facebook
             </a>
           </li>
           <li className={footerNavListItem}>
-            <a href="https://www.gatsbyjs.com/cloud/">About Gatsby Cloud</a>
+            <a href="https://www.instagram.com/fasttechferizaj/">Instagram</a>
           </li>
-          {process.env.GATSBY_DEMO_STORE === "true" && (
-            <li className={footerNavListItem}>
-              <a href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-shopify&utm_campaign=shopify-starter">
-                <img
-                  src="https://www.gatsbyjs.com/deploynow.png"
-                  alt="Deploy to Gatsby Cloud"
-                  height="38"
-                  width="251"
-                />
-              </a>
-            </li>
-          )}
         </ul>
       </nav>
       <div className={copyright}>
-        Copyright &copy; {new Date().getFullYear()} · All rights reserved
+        Copyright &copy; {new Date().getFullYear()} · All rights reserved.  <a href="https://www.pixendrit.com/">
+              Pixendrit
+            </a>
       </div>
     </footer>
   )
