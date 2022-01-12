e6b29d3632ad312fa6db909be645d2df4d81bd57
Endrit <pixendrit@outlook.com>
2022-01-12 03:56:48 +0100

    hmm

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-netlify",
     // Add your Google Analytics ID to the .env file to enable
     // Otherwise, this plugin can be removed
     process.env.GOOGLE_ANALYTICS_ID && {
